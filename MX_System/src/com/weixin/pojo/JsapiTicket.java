3d35f66f9df5154c29e8e1f4336badf2b51dc12b
zw <790454124@qq.com>
2017-07-17 10:24:03 +0800

    999

---
+++ MX_System/src/com/weixin/pojo/JsapiTicket.java
@@ -0,0 +1,23 @@
+package com.weixin.pojo;
+/**
+ * 调用api时的临时票据
+ * @author zw
+ *
+ */
+public class JsapiTicket {
+	private String ticket;//票据
+	private int expires_in;//有效时间
+	public String getTicket() {
+		return ticket;
+	}
+	public void setTicket(String ticket) {
+		this.ticket = ticket;
+	}
+	public int getExpires_in() {
+		return expires_in;
+	}
+	public void setExpires_in(int expires_in) {
+		this.expires_in = expires_in;
+	}
+	
+}
